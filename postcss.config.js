81db0054d152067c75aceb2229eece978b4082c4
Francisco Dias <francisco.ffbhd@gmail.com>
2021-01-03 02:45:35 +0000

    purges css

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,34 @@
-const autoprefixer = require('autoprefixer')
-const cssnano = require('cssnano')
-const postcssImport = require('postcss-import')
-const postcssNested = require('postcss-nested')
-const tailwindCSS = require('tailwindcss')
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcssImport = require("postcss-import");
+const postcssNested = require("postcss-nested");
+const tailwindCSS = require("tailwindcss");
 const purgecss = require("@fullhuman/postcss-purgecss")({
-    content: ["./src/**/*.svelte", "./src/**/*.html"],
-    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  content: ["./src/**/*.svelte", "./src/**/*.html"],
+  defaultExtractor: (content) => {
+    // Capture as liberally as possible, including things like `h-(screen-1.5)`
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+
+    return broadMatches.concat(innerMatches);
+  },
 });
 
 const mode = process.env.NODE_ENV;
-const production = mode === 'production';
+const production = mode === "production";
 
 module.exports = {
-    plugins: [
-        autoprefixer,
-        postcssNested,
-        postcssImport,
-        tailwindCSS(),
-        production && cssnano({
-            preset: [
-                'default',
-                {discardComments: {removeAll: true}},
-            ],
-        }),
-        production && purgecss
-    ].filter(Boolean)
-}
+  plugins: [
+    autoprefixer,
+    postcssNested,
+    postcssImport,
+    tailwindCSS(),
+    production &&
+      cssnano({
+        preset: ["default", { discardComments: { removeAll: true } }],
+      }),
+    production && purgecss,
+  ].filter(Boolean),
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,7 @@ module.exports = {
     extendedFontSizeScale: true,
     applyComplexClasses: true,
   },
-  purge: {
-    // needs to be set if we want to purge all unused
-    // @tailwind/typography styles
-    mode: "all",
-    content: ["./src/**/*.svelte", "./src/**/*.html"],
-  },
+  purge: false,
   theme: {
     container: {},
     extend: {},
